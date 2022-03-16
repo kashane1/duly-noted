a9e499ad2c994376ad9504bc6e70321bba04bdcb
Kashane Sakhakorn Laptop <ksakhakorn@gmail.com>
2022-03-15 21:15:44 -0700

    started working on the delete route but didnt finish yet

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET route for retrieving all the notes on page load
@@ -28,5 +28,36 @@ notes.post('/', (req, res) => {
         res.error('Error in adding note');
     }
 });
+
+notes.delete('/:id', isAuth, (req,res) => {
+    const { id } = req.params;
+    
+    Post.findOneAndDelete({
+        _id: id, 
+        $or: [{author: req.user._id}, {profile: req.user.username}]
+    })
+    .exec((err, post) => {
+        if(err)
+        return res.status(500).json({code: 500, message: 'There was an error deleting the post', error: err})
+        res.status(200).json({code: 200, message: 'Post deleted', deletedPost: post})
+    });
+})
+
+notes.delete('/:id', (req, res) => {
+    const id = req.params;
+    
+    if (id === -1) return res.error('Error in deleting note');
+
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    
+
+    // start here with delete
+
+    // writeToFile = (destination, content) =>
+    // fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    //   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    // );
+})
 
 module.exports = notes;
