a2cf8d983ba72b9d939f0bef82ef186ec996b11b
Kashane Sakhakorn Laptop <ksakhakorn@gmail.com>
2022-03-15 19:12:26 -0700

    initial commit with all the starter code.

---
+++ server.js
@@ -0,0 +1,6 @@
+// starting with some of my notes and thoughts
+
+// the GET /notes can be in this server.js file
+// as well as GET *
+
+// then API routes will be written in the /api/ folder that i created, im thinking index.js
