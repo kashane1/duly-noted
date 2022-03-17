65b3726f80b3d70a578f4bbc96034cb1f0b6c470
Kashane S <ksakhakorn@gmail.com>
2022-03-16 19:10:55 -0700

    saving some changes and moving to my laptop

--- routes/notes.js
+++ routes/notes.js
@@ -29,77 +29,37 @@ notes.post('/', (req, res) => {
     }
 });
 
-// notes.delete('/:id', isAuth, (req,res) => {
-//     const { id } = req.params;
-    
-//     Post.findOneAndDelete({
-//         _id: id, 
-//         $or: [{author: req.user._id}, {profile: req.user.username}]
-//     })
-//     .exec((err, post) => {
-//         if(err)
-//         return res.status(500).json({code: 500, message: 'There was an error deleting the post', error: err})
-//         res.status(200).json({code: 200, message: 'Post deleted', deletedPost: post})
-//     });
-// })
-
 notes.delete('/:id', (req, res) => {
-    const id = req.params;
-    
-    if (id === -1) return res.error('Error in deleting note');
+    const idDeleteObj = req.params;
+    const idDelete = idDeleteObj.id;
+    console.log(idDelete);
+
+    if (idDelete === -1) return res.error('Error in deleting note');
 
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => {
         let inNotesData = JSON.parse(data)
         console.log(inNotesData);
 
-        let outNotesData = inNotesData.map(notes => {
-            if ()
-        })
-        // could use this to create new array:
-        // let carsProperties = cars.map(car => {
-        //     let properties = {
-        //       "capacity": car.capacity,
-        //       "size": "large"
-        //     };
-        //     if (car.capacity <= 5){
-        //       properties['size'] = "medium";
-        //     }
-        //     if (car.capacity <= 3){
-        //       properties['size'] = "small";
-        //     }
-        //     return properties;
-        //    });
-        //    console.log(carsProperties);
-        //    // output:
-        //    // [
-        //    //   { capacity: 7, size: 'large' },
-        //    //   { capacity: 5, size: 'medium' },
-        //    //   { capacity: 5, size: 'medium' },
-        //    //   { capacity: 2, size: 'small' },
-        //    //   ...
-        //    // ]
+        let indexDelete = inNotesData.findIndex(inNotesData.id == idDelete);
+        console.log(indexDelete);
+        inNotesData.splice(indexDelete,1),
 
+        // removeNote(id) {
+        //     // Get all notes, remove the note with the given id, write the filtered notes
+        //     return this.getNotes()
+        //       .then(notes => notes.filter(note => note.id !== parseInt(id)))
+        //       .then(filteredNotes => this.write(filteredNotes));
+        //   }
 
+        
+        console.log(inNotesData);
 
-        writeToFile(allNotes, './db/db.json');
+        writeToFile('./db/db.json', inNotesData);
     });
-    // will need this to delete?
-    // Array.splice(
-    //     {index where to start},
-    //     {how many items to remove},
-    //     {items to add}
-    //   );
 
 
     res.json(`Note deleted successfully 🚀`);
-
-    // start here with delete
-
-    // writeToFile = (destination, content) =>
-    // fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    //   err ? console.error(err) : console.info(`\nData written to ${destination}`)
-    // );
 })
 
 module.exports = notes;
